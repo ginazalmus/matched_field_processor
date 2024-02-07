b65d3daea6e2e74ba60e82b6d4d6c18b7d97763e
ginaz <64552177+ginazalmus@users.noreply.github.com>
2024-02-07 14:40:32 +0100

    Update mvdr.m

--- mvdr.m
+++ mvdr.m
@@ -23,10 +23,9 @@ beta = beta - min(beta(:));
 beta = beta ./ max(beta(:)); 
 beta_norm=beta;
 beta_r0=max(beta_norm);
-%%%Maksimum bulma
-[argvalue, argmax] = max(beta_norm);
 
-%%%Maksimum pointlerin location kayıdı
+½finding the location of the maximum value
+[argvalue, argmax] = max(beta_norm);
 D_max(1,1)=D_new(argmax,1);
 D_max(1,2)=D_new(argmax,2);
 
